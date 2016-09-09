82082588969fad3e59b1c9a38f78e9b0696193c4
Ellery Womack <ellery.womack@cygrp.com>
2016-09-09 14:47:44 -0500

    Added first route

---
+++ srv/controllers/homeController.js
@@ -0,0 +1,7 @@
+var path = require('path');
+
+module.exports.controller = function(app) {
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../../web/views', 'login.html'));
+    });
+}
