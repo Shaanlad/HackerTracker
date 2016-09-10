41b7b45b32764fd8f1764446154b95a75b3d0356
Kartikya Thakur <kartikya.thakur@cygrpdal.local>
2016-09-10 19:21:36 +0530

    Added missing Drop functionality

--- web/controllers/projectController.js
+++ web/controllers/projectController.js
@@ -1,53 +1,75 @@
-angular.module('HackerTracker').controller('projectController', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
+angular.module('HackerTracker').controller('projectController', ['$http', '$scope', '$routeParams', '$mdDialog', function($http, $scope, $routeParams, $mdDialog) {
 
     $scope.project = {};
-    $scope.obj = "Hello";
+    $scope.card = {};
     
     $http.get('project/' + $routeParams.id).then(function(response) {
         console.log(response.data);
         $scope.project = response.data;
     }, function(response) {});
 
-    // $scope.onDragComplete=function(data,evt){
-    //    console.log("drag success, data:", data);
-    // }
-    // $scope.onDropComplete=function(data,evt){
-    //     console.log("drop success, data:", data);
-    // }
+    $scope.centerAnchor = true;
     
-    // // Model to JSON for demo purpose
-    // $scope.$watch('models', function(model) {
-    //     $scope.modelAsJson = angular.toJson(model, true);
-    // }, true);
+    $scope.toggleCenterAnchor = function () {
+        $scope.centerAnchor = !$scope.centerAnchor
+    }
 
-  $scope.centerAnchor = true;
-                $scope.toggleCenterAnchor = function () {
-                    $scope.centerAnchor = !$scope.centerAnchor
-                }
-                //$scope.draggableObjects = [{name:'one'}, {name:'two'}, {name:'three'}];
-                var onDraggableEvent = function (evt, data) {
-                    console.log("128", "onDraggableEvent", evt, data);
-                }
-                $scope.$on('draggable:start', onDraggableEvent);
-               // $scope.$on('draggable:move', onDraggableEvent);
-                $scope.$on('draggable:end', onDraggableEvent);
-                $scope.unassignedCards = [];
-                $scope.inProgressCards = [];
-                $scope.completedCards = [];
-                $scope.onDropComplete = function (containerName, data, evt) {
-                    console.log("127", "$scope", "onDropComplete1", containerName, data, evt);
-                    var index = $scope[containerName].indexOf(data);
-                    if (index == -1)
-                        $scope[containerName].push(data);
-                }
-                $scope.onDragSuccess = function (containerName, data, evt) {
-                    console.log("133", "$scope", "onDragSuccess1", "", evt);
-                    var index = $scope[containerName].indexOf(data);
-                    if (index > -1) {
-                        $scope[containerName].splice(index, 1);
-                    }
-                }
-                var inArray = function (array, obj) {
-                    var index = array.indexOf(obj);
-                }
+    var onDraggableEvent = function (evt, data) {
+        console.log("128", "onDraggableEvent", evt, data);
+    }
+
+    $scope.$on('draggable:start', onDraggableEvent);
+    $scope.$on('draggable:end', onDraggableEvent);
+
+    $scope.unassignedCards = [];
+    $scope.inProgressCards = [];
+    $scope.completedCards = [];
+
+    $scope.onDropComplete = function (containerName, data, evt) {
+        var index = $scope[containerName].indexOf(data);
+        if (index == -1)
+            $scope[containerName].push(data);
+    }
+    $scope.onDragSuccess = function (containerName, data, evt) {
+        var index = $scope[containerName].indexOf(data);
+        if (index > -1) {
+            $scope[containerName].splice(index, 1);
+        }
+    }
+    var inArray = function (array, obj) {
+        var index = array.indexOf(obj);
+    }
+
+    $scope.showCardCreator = function(ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            scope: $scope.$new(),
+            templateUrl: '/views/createCardDialog.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose:true,
+            fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+        })
+        .then(function() {
+            // $scope.create();
+        }, function() {
+            $mdDialog.cancel();
+        });
+    };
+
+    function DialogController($scope, $mdDialog) {
+        $scope.card.name = '';
+        $scope.card.description = '';
+
+        $scope.cancel = function() {
+            $scope.card.name = '';
+            $scope.card.description = '';
+            $mdDialog.cancel();
+        };
+
+        $scope.create = function() {
+            $mdDialog.cancel();            
+        };
+    };
+
 }]);
