13d1c3b4df176a13704486574f037d8236d19946
Ellery Womack <ellery.womack@cygrp.com>
2016-09-09 21:15:33 -0500

    I'm definitely the idiot there.  Themes are working, I forgot how to Angular :(

--- web/app.js
+++ web/app.js
@@ -1,7 +1,4 @@
-var app = angular.module('HackerTracker', ['ngMaterial'])
-.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    .primaryPalette('pink')
-    .accentPalette('orange')
-    .secondaryPalette('blue');
+var app = angular.module('HackerTracker', ['ngMaterial']);
+app.config(function($mdThemingProvider) {
+  $mdThemingProvider.theme('default').dark();
 });
